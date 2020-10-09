6870722361a78ee1170d6d313540e9fb9904ccaf
szilard <szilard@users.noreply.github.com>
2020-10-09 05:33:43 -0700

    update R installation instructions

--- 1-xgboost-R/0-install.R
+++ 1-xgboost-R/0-install.R
@@ -6,4 +6,13 @@ install.packages("ROCR")
 
 install.packages("xgboost")
 
+## until lightgbm gets (back) to CRAN:
+PKG_URL <- "https://github.com/microsoft/LightGBM/releases/download/v3.0.0/lightgbm-3.0.0-r-cran.tar.gz"
+install.packages("remotes")
+remotes::install_url(PKG_URL)
+## more info: https://lightgbm.readthedocs.io/en/latest/R/index.html
+
+install.packages(c("RCurl","h2o"))
+
+
 
