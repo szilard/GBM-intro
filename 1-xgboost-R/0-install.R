5d185b5ef4a1346ce5d15201f7e40e0df6a08f0e
szilard <szilard@users.noreply.github.com>
2021-01-21 02:08:02 -0800

    lightgbm on CRAN

--- 1-xgboost-R/0-install.R
+++ 1-xgboost-R/0-install.R
@@ -4,14 +4,7 @@ install.packages("ggplot2")
 install.packages("ROCR")
 
 
-install.packages("xgboost")
-
-## until lightgbm gets (back) to CRAN:
-PKG_URL <- "https://github.com/microsoft/LightGBM/releases/download/v3.0.0/lightgbm-3.0.0-r-cran.tar.gz"
-install.packages("remotes")
-remotes::install_url(PKG_URL)
-## more info: https://lightgbm.readthedocs.io/en/latest/R/index.html
-
+install.packages(c("xgboost","lightgbm"))
 install.packages(c("RCurl","h2o"))
 
 
